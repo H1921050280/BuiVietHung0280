0c5fe5c06de9263024a76089b5ee0f274a5e5a42
H1921050280 <phamhung235z@gmail.com>
2023-04-24 15:12:16 +0700

    Hung

---
+++ BuiVietHung0280/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiVietHung0280.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
